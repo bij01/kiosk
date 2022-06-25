801e739b1d0ed72347402ebe21876a63cfa485c8
Injoon Baek <bij01@naver.com>
2022-06-25 12:10:42 +0900

    first commit

---
+++ OrderClient.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.event;
+import javax.swing.table.*;
+import javax.swing.border.*;
+
+class OrderClient extends JFrame {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
